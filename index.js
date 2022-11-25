c66b24820c1b10fa779e4666fe8b32707d462096
jerry-kjk <jerrykajik@gmail.com>
2022-11-26 00:06:46 +0300

    Done intro array

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function array(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+function appendCat(name){
+  let copyOfCat = [...cats, "Broom"];
+  return copyOfCat;
+}
+
+function prependCat(name){
+  let prepEnd = ["Arnold", ...cats];
+  return prepEnd;
+}
+
+function removeLastCat(){
+  let removeLast = cats.slice(0,2);
+  return removeLast;
+}
+
+function removeFirstCat(){
+  let removeFirst = cats.slice(1);
+  return removeFirst;
+}
+
